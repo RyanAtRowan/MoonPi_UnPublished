01e192384b2a2854dfbe95c4dfc1cb33e1fb48f4
RyanAtRowan <astralpeaches@gmail.com>
2024-03-03 13:12:38 -0500

    Added area for Blog addition

---
+++ Portfolio/Areas/Blog/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Areas.Blog.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
