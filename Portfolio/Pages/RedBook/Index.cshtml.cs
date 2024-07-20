d2c3540497830ea5c832742167ca96f85a3258ea
RyanAtRowan <astralpeaches@gmail.com>
2024-07-20 14:44:57 -0400

    Created RedBook directory. New _layout routed correctly to allow different appearance. Navigation set up, pages need creation.

---
+++ Portfolio/Pages/RedBook/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages.RedBook
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
