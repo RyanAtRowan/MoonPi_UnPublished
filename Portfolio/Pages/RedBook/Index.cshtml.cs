997691771d5f9c417199c7d89a360831a5c9e8ff
RyanAtRowan <astralpeaches@gmail.com>
2024-07-20 16:16:07 -0400

    The index page for RedBook is now functional. Properly displays top 10 scrolls based on price. (All scrolls are 0 right now, next is to implement Identity and add favorites / modification options

--- Portfolio/Pages/RedBook/Index.cshtml.cs
+++ Portfolio/Pages/RedBook/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portfolio.Data;
+using Portfolio.Models;
 
 namespace Portfolio.Pages.RedBook
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Scroll> Scrolls { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            Scrolls = _db.Scrolls.OrderByDescending(s => s.Price).Take(10).ToList();
         }
     }
 }
