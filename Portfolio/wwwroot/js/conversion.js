209a69149f7fb8a593dfad861dcc9e50c6e1cb1d
RyanAtRowan <astralpeaches@gmail.com>
2024-07-25 01:26:52 -0400

    Added logic to convert the raw price value into abbreviated format (like in the preview in edit) for results and top 10

---
+++ Portfolio/wwwroot/js/conversion.js
@@ -0,0 +1,22 @@
+﻿function convertNumber() {
+    var input = document.getElementById("numberInput").value;
+    var converted = formatNumber(input);
+    document.getElementById("convertedValue").innerText = converted;
+}
+
+function formatNumber(num) {
+    num = Number(num);
+    if (num >= 1000000000) {
+        return (num / 1000000000).toFixed(1) + 'b';
+    }
+
+    else if (num >= 1000000) {
+        return (num / 1000000).toFixed(1) + 'm';
+    }
+
+    else if (num >= 1000) {
+        return (num / 1000).toFixed(1) + 'k';
+    } else {
+        return num.toString();
+    }
+}
