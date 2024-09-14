f53939ad28080a3ff69eda6b34b77f568211aa58
RyanAtRowan <astralpeaches@gmail.com>
2024-09-13 22:09:20 -0400

    Adjusted Fade-in script to be stand-alone file for scalability and potential future use

---
+++ Portfolio/wwwroot/js/fade-in.js
@@ -0,0 +1,8 @@
+﻿var element = document.querySelector('#fade-in');
+
+element.style.opacity = 0;
+
+void element.offsetWidth; // This forces a browser reflow
+
+element.style.transition = "opacity 0.5s linear";
+element.style.opacity = 1;
