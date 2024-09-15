73a8e8d3da53cdab8fdfa5810b50e097e2b9f14d
RyanAtRowan <astralpeaches@gmail.com>
2024-09-15 13:36:09 -0400

    Added AccountController so that Demo can be a nav button. This is much better design and makes it feel much safer to test out the software.
    Removed the Demo button from the login razor page.

---
+++ Portfolio/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+
+namespace Portfolio.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, ILogger<AccountController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> DemoLogin()
+        {
+
+            // Assuming you have a predefined demo user (e.g., "demo@user.com")
+            var demoEmail = "demo@user.com";
+            var demoPassword = "Demo123!"; // You can set a default password
+
+            // Sign in the demo user
+            var result = await _signInManager.PasswordSignInAsync(demoEmail, demoPassword, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("Demo user logged in.");
+                return LocalRedirect("~/RedBook/Index"); // Redirect to your main page or demo area
+            }
+            else
+            {
+                _logger.LogWarning("Demo login attempt failed.");
+                return RedirectToAction("Login", "Account"); // Redirect to login page if demo login fails
+            }
+        }
+    }
+}
