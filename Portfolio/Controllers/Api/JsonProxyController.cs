2b9485a2a7b46a18022a7f45fc40c6bfa562b62b
RyanAtRowan <astralpeaches@gmail.com>
2024-09-19 00:45:15 -0400

    Corrected clean slate bug and implemented API to dodge CORS issue with mobile devices.

---
+++ Portfolio/Controllers/Api/JsonProxyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JsonProxyController : Controller
+    {
+        [HttpGet("fetchJson")]
+        public IActionResult GetJsonData()
+        {
+            string jsonPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Assets", "DropDownData.json");
+
+            if (!System.IO.File.Exists(jsonPath))
+            {
+                return NotFound("JSON file not found.");
+            }
+
+            var jsonData = System.IO.File.ReadAllText(jsonPath);
+
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            return Content(jsonData, "application/json");
+        }
+    }
+}
