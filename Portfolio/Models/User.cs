6257c4db664ed0879fa511ecc31b79518ae491b6
RyanAtRowan <astralpeaches@gmail.com>
2024-09-28 20:16:39 -0400

    Added documentation for Favorite.cs model

--- Portfolio/Models/User.cs
+++ Portfolio/Models/User.cs
@@ -2,14 +2,32 @@
 
 namespace Portfolio.Models
 {
+    /// <summary>
+    /// Represents a user in the Red Book system.
+    /// Contains the user's username, password, and their list of favorite scrolls.
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Unique identifier for the user.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The username of the user.
+        /// </summary>
         [Required]
         public string Username { get; set; }
+
+        /// <summary>
+        /// The password for the user.
+        /// </summary>
         [Required]
         public string Password { get; set; }
 
+        /// <summary>
+        /// A collection of the user's favorite scrolls.
+        /// </summary>
         public ICollection<Favorite> Favorites { get; set; }
     }
 }
